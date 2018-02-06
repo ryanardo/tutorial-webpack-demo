aec53595cef9b91164aeaa7c17b0e325b8a1ce19
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-06 15:27:22 -0800

    Add image to the div element created in the index.js file.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
-	var element = document.createElement('div');
+	/* This line creates the html div element that webpack gets to control. */
+	var element = document.createElement('div'); //If div was changed to body, icon.png, would be the background for the entire html document.
 
 	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
+
+	// Add image to our existing div.
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 }

77cac7feaba7cda0298b2117d29a568c50bb4e38
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-06 15:22:03 -0800

    Update the webpack config file to use the NPM file-loader package on images in our project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: './src/index.js', //source file
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		filename: 'bundle.js', //end filepoint
+		path: path.resolve(__dirname, 'dist') //filepath
 	},
 	module: {
 		rules: [
+			// Will load our CSS files into the index.html file.
 			{
-				test: /\.css$/,
+				test: /\.css$/, //regex for files to load
 				use: [
-					'style-loader',
+					'style-loader', //what loader(s) to shove them through, in order
 					'css-loader'
+				]
+			},
+			// Will load our image files into the index.html file.
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
 				]
 			}
 		]

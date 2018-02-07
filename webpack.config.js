77936ac319f83eaa14344434c8e9530a5a0177cd
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-06 16:20:07 -0800

    Update webpack config file to take in multiple source files and output multiple bundle files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,13 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js', //source file
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
 	output: {
-		filename: 'bundle.js', //end filepoint
+		filename: '[name].bundle.js', //end filepoint
 		path: path.resolve(__dirname, 'dist') //filepath
 	},
-	module: {
-		rules: [
-			// Will load our CSS files into the index.html file.
-			{
-				test: /\.css$/, //regex for files to load
-				use: [
-					'style-loader', //what loader(s) to shove them through, in order
-					'css-loader'
-				]
-			},
-			// Will load our image files into the index.html file.
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader'
-				]
-			}
-		]
-	}
+
 };

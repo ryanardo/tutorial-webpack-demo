cb88e3d4013b1421719bc3911751b545fbcb2ece
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-07 08:20:52 -0800

    Add HtmlWebpackPlugin to the webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //Will create a new index.html file with updated dependencies.
 
 module.exports = {
 	entry: {
 		app: './src/index.js',
 		print: './src/print.js'
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
 	output: {
 		filename: '[name].bundle.js', //end filepoint
 		path: path.resolve(__dirname, 'dist') //filepath

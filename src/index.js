12cf64c9c93c5b44691654177d44bd490acfcdf4
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-07 10:44:24 -0800

    Update index.js file to run logic to determine if you are in production or development enviornment.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ The import statements below import ALL of the external code neccessary to make t
 */
 import { cube } from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in the development mode!');
+}
+
 function component() {
 	/* This line creates the html div element that webpack gets to control. */
 	var element = document.createElement('pre');

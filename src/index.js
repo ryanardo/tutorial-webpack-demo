22876ed71ba724c71541f38d1dc6b6bb89d6fc49
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-06 16:18:42 -0800

    Update index.js to include the 'Click me!' button in the index.html file.

--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@
 The import statements below import ALL of the external code neccessary to make the corresponding HTML file(s) run properly.
 */
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
 	/* This line creates the html div element that webpack gets to control. */
-	var element = document.createElement('div'); //If div was changed to body, icon.png, would be the background for the entire html document.
-
-	// Lodash, now imported by this script
-	/* HERE IS WHAT LODASH DOES!!!!!!!!
-	var words = [Hello, webpack];
-	var displayWords = words.join(' ');
-	element.innerHTML = displayWords;
-	*/
+	var element = document.createElement('div'); //If div was changed to body, icon.png, would be the
+	var btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
 
-	// Add image to our existing div.
-	var myIcon = new Image();
-	myIcon.src = Icon;
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
 
-	element.appendChild(myIcon);
+	element.appendChild(btn);
 
 	return element;
 }

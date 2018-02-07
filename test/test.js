bf7a510c5437d43efc5528337d9480ab2e470514
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-07 14:58:30 -0800

    create test for math.js, demostrating mocha.
    test command: npm run test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { square, cube } from "../src/math.js";
+var assert = require('assert');
+
+
+describe('math', function () {
+	describe('square', function () {
+		it('should return square of input value', function () {
+			assert.equal(25, square(5));
+		});
+	});
+});

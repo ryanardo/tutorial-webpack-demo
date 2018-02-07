a4f28ced26a2a5d32721ccf5e63bcf4900626f15
Ryan G Ferris <ryanglenferris@gmail.com>
2018-02-07 15:42:45 -0800

    karma1
    karma1

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,26 @@
+const testsContext = require.context(".", true, /.test$/);
+
 module.exports = function (config) {
 	config.set({
+		basePath: '../..',
+		frameworks: ['jasmine'],
 		files: [
 			{ pattern: 'test/*_test.js', watched: false },
 			{ pattern: 'test/**/*_test.js', watched: false }
     ],
 		preprocessors: {
-			'test/*_test.js': ['webpack'],
-			'test/**/*_test.js': ['webpack']
+			'test/*.test.js': ['webpack'],
+			'test/**/*.test.js': ['webpack']
+			'test/test.index.js': ['webpack', 'sourcemap']
 		},
+		testsContext.keys().forEach(testsContext);
 		webpack: {
 			// Any custom webpack configuration...
 		},
 		webpackMiddleware: {
 			// Any custom webpack-dev-middleware configuration...
+			stats: 'errors-only'
+			devtool: 'inline-source-map'
 		}
 	});
 };

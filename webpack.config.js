38b93a599b8af307106d21226b6466fa20948498
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-07 09:50:43 -0800

    First attempt at hot moduler replacement.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //Will create a new index.html file with updated dependencies.
 const CleanWebpackPlugin = require('clean-webpack-plugin'); //Will clean the 'dist' folder on each new build command.
+const webpack = require('webpack'); //Hot Module Replacement is built into webpack by default.
 
 module.exports = {
 	entry: {
-		app: './src/index.js',
-		print: './src/print.js'
+		app: './src/index.js'
 	},
 	devtool: 'inline-source-map', //Source mapp tool included with JavaScript.
 	devServer: {
 		contentBase: './dist'
+		hot: true
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
-			title: 'Fart Management'
-		})
+			title: 'Hot Module Replacement'
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
 	],
 	output: {
 		filename: '[name].bundle.js', //end filepoint

42d4ba0950ed5d176e1eafc4d04bf6b615570ef7
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-07 16:48:24 -0800

    Add notes to webpack config files.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,30 @@
+/*
+NPM Packages necessary to make your code run/function.
+*/
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	/*
+	Location of your source code.
+	> You can have more than 1 source code file.
+	*/
 	entry: {
 		app: './src/index.js'
 	},
+	/*
+
+	*/
 	plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
 			title: 'Production'
 		})
   ],
+	/*
+	Location of your output bundled code (a bundle will be created for each of your source code files).
+	*/
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
  const merge = require('webpack-merge');
  const common = require('./webpack.common.js');
-
+ /*
+ If from an OO track, the 'merge' constant is a work around for JS b/c it does not have 'proper' object inheritance.
+ */
  module.exports = merge(common, {
  	devtool: 'inline-source-map',
  	devServer: {
